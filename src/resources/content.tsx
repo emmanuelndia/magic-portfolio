6d751e3ce2df6311f018a1c854539996948183eb
Emmanuel N'dia <emmanuellevi.ndia@gmail.com>
2026-02-02 13:06:08 +0000

    Ajout de favicon et renommage du titre du site

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -58,7 +58,7 @@ const home: Home = {
   path: "/",
   image: "/images/og/home.jpg",
   label: "Home",
-  title: `${person.name}'s Portfolio`,
+  title: `${person.name} portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
   headline: <>Building bridges between design and code</>,
   featured: {
